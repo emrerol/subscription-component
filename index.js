95a564b07cdd8767ef159251fa9d885c0573d23d
Emre Can Erol <erolemrecan@outlook.com>
2022-07-14 22:23:29 +0300

    last commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const tikla = document.getElementById("tikla");
+
+tikla.addEventListener("click", () =>
+  alert(
+    "Bu kodlamadaki amacım responsive tasarım değildir. Yalnızca verilen tasarımı, mümkün olduğunca kendi değerine yakın şekilde kodlamayı amaçladım"
+  )
+);
