4a0840d7c78e7b2685a993f6c4d6c9b207491e34
Emre Can Erol <erolemrecan@outlook.com>
2022-07-21 19:46:45 +0300

    info butonu kaldırıldı

--- index.js
+++
@@ -1,7 +0,0 @@
-const tikla = document.getElementById("tikla");
-
-tikla.addEventListener("click", () =>
-  alert(
-    "Bu kodlamadaki amacım responsive tasarım değildir. Yalnızca verilen tasarımı, mümkün olduğunca kendi değerine yakın şekilde kodlamayı amaçladım"
-  )
-);
